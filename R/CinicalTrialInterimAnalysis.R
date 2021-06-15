76d9b6ab0596f9c2c8bc4fb5d8b0805579072f01
Thompson, Zachary J <zachary.thompson@moffitt.org>
2021-06-15 15:22:24 -0400

    modified the names of the functions inside the package

---
+++ R/CinicalTrialInterimAnalysis.R
@@ -0,0 +1,25 @@
+#' @name CinicalTrialInterimAnalysis
+
+#' @title Clinical Trial Interim Report
+
+#' @description This function will run a Shiny application
+
+#' @export
+
+
+
+  CinicalTrialInterimAnalysis <- function() {
+
+  appDir <- system.file("shiny-examples", "myapp", package = "CinicalTrialInterimAnalysis")
+
+  if (appDir == "") {
+
+    stop("Could not find example directory. Try re-installing `CinicalTrialInterimAnalysis`.", call. = FALSE)
+
+  }
+
+
+
+  shiny::runApp(paste(appDir,'/app.R',sep=''), launch.browser =T,host = getOption( "127.0.0.1"))
+
+}
